c870301c4e2cabeb3fc9f67fb40f98fd2aad1e8e
kowsalyabe19 <kowsalyadpm98@gmail.com>
2022-03-23 12:52:04 +0530

    Added dependency

---
+++ GameRegistrationApp/src/main/java/com/game/RegistrationApp/GameRegistrationApp/GameRegistrationAppApplication.java
@@ -0,0 +1,13 @@
+package com.game.RegistrationApp.GameRegistrationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRegistrationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameRegistrationAppApplication.class, args);
+	}
+
+}
---
+++ GameRegistrationApp/src/test/java/com/game/RegistrationApp/GameRegistrationApp/GameRegistrationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.RegistrationApp.GameRegistrationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameRegistrationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
